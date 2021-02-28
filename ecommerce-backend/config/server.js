3ef15aeea3f98ac352d4441a6e48e2e9a2fbd1f7
Bao Dang <danglequocbao001@gmail.com>
2021-02-28 23:26:18 +0700

    initial commit

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '82311290c5e0389c499f106689f7fe8e'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8f6be915-cd41-4229-834f-fbce1dcee36d'
+};
